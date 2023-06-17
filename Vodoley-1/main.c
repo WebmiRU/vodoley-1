60fa29b24818bb736a6ff93973cb2e48648c7d56
E.Wolf <e.wolf@webmi.ru>
2023-06-17 19:15:14 +0300

    Требуется оптимизация прерывания по таймеру

--- Vodoley-1/main.c
+++ Vodoley-1/main.c
@@ -16,10 +16,10 @@
 #define BTN_PRESS_LONG 200
 #define BUZZER_BEEP_SHORT 25
 #define BUZZER_BEEP_LONG 200
-#define PUMP_TARGET_0 90 - FLOW_INERTION // 200ml
-#define PUMP_TARGET_1 180 - FLOW_INERTION // 400ml
-#define PUMP_TARGET_2 270 - FLOW_INERTION // 600ml
-#define PUMP_TARGET_3 450 - FLOW_INERTION // 1000ml
+#define PUMP_TARGET_0 90 // 200ml
+#define PUMP_TARGET_1 180 // 400ml
+#define PUMP_TARGET_2 270 // 600ml
+#define PUMP_TARGET_3 450 // 1000ml
 
 #define BTN_0 (PIND & (1 << 0))
 #define BTN_1 (PIND & (1 << 1))
@@ -28,18 +28,14 @@
 #define BTN_4 (PIND & (1 << 7))
 #define BUZZER_0 (PIND & (1 << 3))
 #define PUMP_0 (PIND & (1 << 4))
+#define REST 8505 // Остаток воды бутылке (кол-во в импульсах)
+#define ML_PER_IMPULSE 0.22222222 // Кол-во миллилитров жидкости на 1 импульс счётчика
 
 uint32_t counter = 0;
+uint32_t rest = REST; // Остаток воды в бутылке
 uint8_t dots = 0b0100;
 uint32_t pump_target = 0;
 uint8_t buzzer_on_counter = 0;
-//
-//volatile uint8_t t0 = 0;
-//#define T0_OVF 10
-
-//uint32_t button_up = {0, 0, 0, 0, 0};
-
-//uint32_t button_up[] = {0, 0, 0, 0, 0};
 uint8_t button_down = 0b00000;
 uint8_t button_up = 0b00000;
 uint8_t button_state = 0b00000;
@@ -47,8 +43,6 @@ uint8_t button_press_short = 0b00000;
 uint8_t button_press_reg = 0b00000;
 uint32_t button_press_counter[] = {0, 0, 0, 0, 0};
 uint32_t button_press_counter_global = 0;
-	
-uint32_t t1 = 1;
 
 void display(uint32_t n, uint8_t dots)  {
 	//uint32_t n = counter;
@@ -287,12 +281,9 @@ ISR(TIMER0_OVF_vect)
 }
 
 ISR(INT0_vect)
-{
-	counter++;
-	
-	if (pump_target > 0) {
-		pump_target--;
-	}
+{	
+	if (pump_target > 0) pump_target--;
+	rest--;
 }
 
 
@@ -336,13 +327,14 @@ int main(void)
 	init();	
 	
 	while (1) {
-		display(counter, dots);
+		display(rest * ML_PER_IMPULSE, dots);
+		//display(pump_target, dots);
 		
-		//if (pump_target > 0) {
-			//pump_on();
-		//} else {
-			//pump_off();
-		//}
+		if (pump_target > 0) {
+			pump_on();
+		} else {
+			pump_off();
+		}
 		
 		if (buzzer_on_counter > 0) {
 			PORTD |= (1 << 3);
@@ -351,7 +343,7 @@ int main(void)
 		}		
 		
 		switch (button_press_reg) {
-			// Длительные нажатия						
+			// Длительные нажатия
 			case 0b10000001: // Кнопка 0
 			counter+=100;
 			break;
@@ -361,28 +353,30 @@ int main(void)
 			break;						
 			
 			case 0b10000011: // Кнопки 0+1
-			counter += 1000;
+			rest = REST;
+			beep_long();
 			break;
 			
 			case 0b10000101:  // Кнопки 0+2
 			counter+=500;
 			break;
 			
+			
 			// Короткие нажатия
 			case 0b00000001: // Кнопка 0
-			counter+=1;
+			pump_target = PUMP_TARGET_0 - FLOW_INERTION;
 			break;
 			
 			case 0b00000010: // Кнопка 1
-			counter+=2;
+			pump_target = PUMP_TARGET_1 - FLOW_INERTION;
 			break;
 			
 			case 0b00000100: // Кнопка 2
-			counter+=3;
+			pump_target = PUMP_TARGET_2 - FLOW_INERTION;
 			break;
 			
 			case 0b00001000: // Кнопка 3
-			counter+=4;
+			pump_target = PUMP_TARGET_3 - FLOW_INERTION;
 			break;
 		}
 		
